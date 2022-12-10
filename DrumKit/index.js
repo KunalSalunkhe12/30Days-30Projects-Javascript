579d7c5fc35d13a5b66f43cf2b198ff9f4da4c38
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2022-12-11 00:37:49 +0530

    Modify index.htm & index.js

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 
 const playSound = (keyCode)=>{
     
@@ -34,40 +32,3 @@ keys.forEach((key) =>{
         playSound(keyClick);
     })
 });
-=======
-
-
-const playSound = (keyCode)=>{
-    
-    let audio = document.querySelector(`audio[data-key="${keyCode}"`);
-    audio.currentTime = 0;
-    audio.play();
-
-
-    let key = document.querySelector(`.key[data-key="${keyCode}"`);
-    key.classList.add('playing');
-    
-    setTimeout(()=>{
-        key.classList.remove('playing')
-    }, 70)
-}
-
-
-
-
-window.addEventListener('keypress' , (e)=>{
-    keyPress = (e.key);
-    playSound(keyPress);
-
-}); 
-   
-
-
-let keys = document.querySelectorAll('.key')
-keys.forEach((key) =>{
-    key.addEventListener('click' , (e)=>{
-        keyClick = (e.target.innerHTML).toLowerCase()
-        playSound(keyClick);
-    })
-});
->>>>>>> 61dc4e1 (Add css folder)
