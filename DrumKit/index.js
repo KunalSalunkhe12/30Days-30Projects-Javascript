e033ef9eaf19ea99dbd5027ce03848b126ad2f39
Kunal Rajesh Salunkhe <salunkhekunal594@gmail.com>
2022-12-11 00:30:50 +0530

    Created Drumkit

---
+++ DrumKit/index.js
@@ -0,0 +1,73 @@
+<<<<<<< HEAD
+
+
+const playSound = (keyCode)=>{
+    
+    let audio = document.querySelector(`audio[data-key="${keyCode}"`);
+    audio.currentTime = 0;
+    audio.play();
+
+
+    let key = document.querySelector(`.key[data-key="${keyCode}"`);
+    key.classList.add('playing');
+    
+    setTimeout(()=>{
+        key.classList.remove('playing')
+    }, 70)
+}
+
+
+
+
+window.addEventListener('keypress' , (e)=>{
+    keyPress = (e.key);
+    playSound(keyPress);
+
+}); 
+   
+
+
+let keys = document.querySelectorAll('.key')
+keys.forEach((key) =>{
+    key.addEventListener('click' , (e)=>{
+        keyClick = (e.target.innerHTML).toLowerCase()
+        playSound(keyClick);
+    })
+});
+=======
+
+
+const playSound = (keyCode)=>{
+    
+    let audio = document.querySelector(`audio[data-key="${keyCode}"`);
+    audio.currentTime = 0;
+    audio.play();
+
+
+    let key = document.querySelector(`.key[data-key="${keyCode}"`);
+    key.classList.add('playing');
+    
+    setTimeout(()=>{
+        key.classList.remove('playing')
+    }, 70)
+}
+
+
+
+
+window.addEventListener('keypress' , (e)=>{
+    keyPress = (e.key);
+    playSound(keyPress);
+
+}); 
+   
+
+
+let keys = document.querySelectorAll('.key')
+keys.forEach((key) =>{
+    key.addEventListener('click' , (e)=>{
+        keyClick = (e.target.innerHTML).toLowerCase()
+        playSound(keyClick);
+    })
+});
+>>>>>>> 61dc4e1 (Add css folder)
